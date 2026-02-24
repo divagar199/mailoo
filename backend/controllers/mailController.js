64a3aca315ab889f5a5e02f3d56f85578b38fa34
divagar199 <divagar.m.msc.cs@gmail.com>
2026-02-25 00:59:55 +0530

    fix: switch SMTP to port 587 STARTTLS for Render compatibility

--- backend/controllers/mailController.js
+++ backend/controllers/mailController.js
@@ -3,11 +3,17 @@ const XLSX = require('xlsx');
 const EmailHistory = require('../models/EmailHistory');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 15000,
 });
 
 exports.sendMail = async (req, res) => {
